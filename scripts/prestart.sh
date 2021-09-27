fbc1179d223f9f8ee9fe656946ac8f9405e4d02b
Leonardo Monteiro <decastromonteiro@gmail.com>
2021-09-27 00:24:07 -0300

    added gunicorn log as structlog

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -10,4 +10,11 @@ alembic upgrade head
 
 echo "PostgreSQL started"
 
+echo "Wait for logstash service to be up"
+while ! nc -z -u logstash_agent 12201; do
+    sleep 0.1
+done
+
+echo "Logstash Agent is UP, initializing FastAPI service"
+
 exec "$@"
